cb7dd4be6064529f5d2f20f0282e05b0942121d8
Ryan Ashcraft <ryan@ryanashcraft.me>
2019-09-08 16:55:29 -0700

    Upgrade Amplitude sdk version to 5.3.0

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -35,10 +35,11 @@ exports.onRenderBody = (
       : `true`
   }) {
     (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
-    ;r.type="text/javascript";r.async=true
-    ;r.src="https://cdn.amplitude.com/libs/amplitude-4.5.2-min.gz.js"
-    ;r.onload=function(){if(e.amplitude.runQueuedFunctions){
-    e.amplitude.runQueuedFunctions()}else{
+    ;r.type="text/javascript"
+    ;r.integrity="sha384-a+mq7tiLwde/00Oc7avFHLn/ttGfdAq1rtZc7u97SEzIiyYoT2IsOKWCkAThwdEu"
+    ;r.crossOrigin="anonymous";r.async=true
+    ;r.src="https://cdn.amplitude.com/libs/amplitude-5.3.0-min.gz.js"
+    ;r.onload=function(){if(!e.amplitude.runQueuedFunctions){
     console.log("[Amplitude] Error: could not load SDK")}}
     ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
     ;function s(e,t){e.prototype[t]=function(){
@@ -49,10 +50,7 @@ exports.onRenderBody = (
     ;return this}
     ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
     ;for(var p=0;p<l.length;p++){s(c,l[p])}n.Revenue=c
-    ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut",
-    "setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify",
-    "clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","logEventWithTimestamp",
-    "logEventWithGroups","setSessionId","resetSessionId"]
+    ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
     ;function v(e){function t(t){e[t]=function(){
     e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}
     for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){
