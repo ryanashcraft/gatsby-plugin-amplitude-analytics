423b51ac58d48a34717f44670611085d6b0e816c
Ryan Ashcraft <ryan@ryanashcraft.me>
2020-07-08 10:51:33 -0700

    Update to Amplitude-JavaScript 7.1.0

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -38,9 +38,9 @@ exports.onRenderBody = (
   }) {
     (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
     ;r.type="text/javascript"
-    ;r.integrity="sha384-a+mq7tiLwde/00Oc7avFHLn/ttGfdAq1rtZc7u97SEzIiyYoT2IsOKWCkAThwdEu"
+    ;r.integrity="sha384-cukXwabQy+j/QA1+RqiXSzxhgQg5Rrn3zVszlwH3pWj/bXJxlA8Ge7NhcD6vP2Ik"
     ;r.crossOrigin="anonymous";r.async=true
-    ;r.src="https://cdn.amplitude.com/libs/amplitude-5.3.0-min.gz.js"
+    ;r.src="https://cdn.amplitude.com/libs/amplitude-7.1.0-min.gz.js"
     ;r.onload=function(){if(!e.amplitude.runQueuedFunctions){
     console.log("[Amplitude] Error: could not load SDK")}}
     ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
@@ -48,11 +48,11 @@ exports.onRenderBody = (
     this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this}}
     var o=function(){this._q=[];return this}
     ;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
-    ;for(var u=0;u<a.length;u++){s(o,a[u])}n.Identify=o;var c=function(){this._q=[]
+    ;for(var c=0;c<a.length;c++){s(o,a[c])}n.Identify=o;var u=function(){this._q=[]
     ;return this}
     ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
-    ;for(var p=0;p<l.length;p++){s(c,l[p])}n.Revenue=c
-    ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
+    ;for(var p=0;p<l.length;p++){s(u,l[p])}n.Revenue=u
+    ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","enableTracking","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
     ;function v(e){function t(t){e[t]=function(){
     e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}
     for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){
