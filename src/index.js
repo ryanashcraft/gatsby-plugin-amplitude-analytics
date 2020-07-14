a54060344012dad1818440527f2d8aef32a55380
David <dfvalero@users.noreply.github.com>
2020-07-14 18:34:34 +0200

    Add onMouseDown logic for middle clicks, and refactor onClick logic

--- src/index.js
+++ src/index.js
@@ -1,52 +1,48 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-function OutboundLink(props) {
-  return (
-    <a
-      {...props}
-      onClick={e => {
-        if (typeof props.onClick === `function`) {
-          props.onClick()
-        }
-        let redirect = true
-        if (
-          e.button !== 0 ||
-          e.altKey ||
-          e.ctrlKey ||
-          e.metaKey ||
-          e.shiftKey ||
-          e.defaultPrevented
-        ) {
-          redirect = false
-        }
-        if (props.target && props.target.toLowerCase() !== `_self`) {
-          redirect = false
-        }
-        if (typeof window.amplitude === 'object') {
-          window.amplitude.getInstance().logEvent(window.amplitudeEventTypes.outboundLinkClick, {
-            href: props.href,
-          }, () => {
-            if (redirect) {
-              document.location = props.href
-            }
-          })
-        } else {
-          if (redirect) {
-            document.location = props.href
-          }
-        }
+const NEWTAB = '_blank';
+const MIDDLE_CLICK = 1;
 
-        return false
-      }}
-    />
-  )
+function OutboundLink({ href, target, onClick, onMouseDown, children, ...rest }) {
+  const handleClick = e => {
+    const sameTarget = target !== NEWTAB;
+    const normalClick = !(e.ctrlKey || e.shiftKey || e.metaKey || e.button === MIDDLE_CLICK);
+
+    if (sameTarget && normalClick) {
+      e.preventDefault();
+      window.amplitude.getInstance().logEvent(window.amplitudeEventTypes.outboundLinkClick, { href }, () => {
+        window.location.href = href;
+      });
+    } else {
+      window.amplitude.getInstance().logEvent(window.amplitudeEventTypes.outboundLinkClick, { href });
+    }
+
+    onClick && onClick(e);
+  };
+
+  // Adding mouseDown handler to track MIDDLE_CLICK, because MIDDLE_CLICK doesn't reach onClick event
+  const handleMouseDown = e => {
+    if (e.button === MIDDLE_CLICK) {
+      window.amplitude.getInstance().logEvent(window.amplitudeEventTypes.outboundLinkClick, { href });
+    }
+
+    onMouseDown && onMouseDown(e);
+  };
+
+  return (
+    <a href={href} target={target} onClick={handleClick} onMouseDown={handleMouseDown} {...rest}>
+      {children}
+    </a>
+  );
 }
 
 OutboundLink.propTypes = {
   href: PropTypes.string,
   target: PropTypes.string,
   onClick: PropTypes.func,
+  onMouseDown: PropTypes.func,
+  children: PropTypes.element,
 }
 
 export { OutboundLink }
