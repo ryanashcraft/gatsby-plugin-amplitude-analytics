6351a12f5db14f95bfb8273beb14735cd7336a73
David <dfvalero@users.noreply.github.com>
2020-07-15 17:43:17 +0200

    rename props and refactor onClick and onMouse logic

--- src/index.js
+++ src/index.js
@@ -4,32 +4,36 @@ import PropTypes from "prop-types"
 const NEWTAB = '_blank';
 const MIDDLE_CLICK = 1;
 
-function OutboundLink({ eventName, eventDetails, href, target, onClick, onMouseDown, children, ...rest }) {
+function OutboundLink({ eventType, eventProperties, href, target, onClick, onMouseDown, children, ...rest }) {
   const handleClick = e => {
-    const amplitudeEventName = eventName || window.amplitudeEventTypes.outboundLinkClick;
-    const amplitudeDetails = Object.assign({ href: href }, eventDetails)
+    const amplitudeEventType = eventType || window.amplitudeEventTypes.outboundLinkClick;
+    const amplitudeDetails = Object.assign({ href: href }, eventProperties)
     const sameTarget = target !== NEWTAB;
     const normalClick = !(e.ctrlKey || e.shiftKey || e.metaKey || e.button === MIDDLE_CLICK);
 
     if (sameTarget && normalClick) {
       e.preventDefault();
-      window.amplitude.getInstance().logEvent(amplitudeEventName, amplitudeDetails, () => {
+      window.amplitude.getInstance().logEvent(amplitudeEventType, amplitudeDetails, () => {
         window.location.href = href;
       });
     } else {
-      window.amplitude.getInstance().logEvent(amplitudeEventName, amplitudeDetails);
+      window.amplitude.getInstance().logEvent(amplitudeEventType, amplitudeDetails);
     }
 
-    onClick && onClick(e);
+    if (onClick) {
+      onClick(e);
+    }
   };
 
   // Adding mouseDown handler to track MIDDLE_CLICK, because MIDDLE_CLICK doesn't reach onClick event
   const handleMouseDown = e => {
     if (e.button === MIDDLE_CLICK) {
-      window.amplitude.getInstance().logEvent(amplitudeEventName, amplitudeDetails);
+      window.amplitude.getInstance().logEvent(amplitudeEventType, amplitudeDetails);
     }
 
-    onMouseDown && onMouseDown(e);
+    if (onMouseDown) {
+      onMouseDown(e);
+    }
   };
 
   return (
@@ -41,7 +45,7 @@ function OutboundLink({ eventName, eventDetails, href, target, onClick, onMouseD
 
 OutboundLink.propTypes = {
   eventName: Proptypes.string,
-  eventDetails: Proptypes.any,
+  eventProperties: Proptypes.any,
   href: PropTypes.string,
   target: PropTypes.string,
   onClick: PropTypes.func,
