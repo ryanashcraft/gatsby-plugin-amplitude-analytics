699107277b7bb655c40fbadc767d7e1c0f1c7746
David <dfvalero@users.noreply.github.com>
2020-07-15 19:12:50 +0200

    remove onMouseDown custom logic

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 const NEWTAB = '_blank';
 const MIDDLE_CLICK = 1;
 
-function OutboundLink({ eventType, eventProperties, href, target, onClick, onMouseDown, children, ...rest }) {
+function OutboundLink({ eventType, eventProperties, href, target, onClick, children, ...rest }) {
   const handleClick = e => {
     const amplitudeEventType = eventType || window.amplitudeEventTypes.outboundLinkClick;
     const amplitudeDetails = Object.assign({ href: href }, eventProperties)
@@ -25,19 +25,8 @@ function OutboundLink({ eventType, eventProperties, href, target, onClick, onMou
     }
   };
 
-  // Adding mouseDown handler to track MIDDLE_CLICK, because MIDDLE_CLICK doesn't reach onClick event
-  const handleMouseDown = e => {
-    if (e.button === MIDDLE_CLICK) {
-      window.amplitude.getInstance().logEvent(amplitudeEventType, amplitudeDetails);
-    }
-
-    if (onMouseDown) {
-      onMouseDown(e);
-    }
-  };
-
   return (
-    <a href={href} target={target} onClick={handleClick} onMouseDown={handleMouseDown} {...rest}>
+    <a href={href} target={target} onClick={handleClick} {...rest}>
       {children}
     </a>
   );
@@ -49,7 +38,6 @@ OutboundLink.propTypes = {
   href: PropTypes.string,
   target: PropTypes.string,
   onClick: PropTypes.func,
-  onMouseDown: PropTypes.func,
   children: PropTypes.element,
 }
 
